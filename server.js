15e36a533dff4b972e5a03aa77664114809fc6fb
Vitya Machine <vatamanenko9@gmail.com>
2023-03-22 16:23:58 +0200

    add header handler

--- server.js
+++ server.js
@@ -42,8 +42,15 @@ class Server {
     const formatsLogger =
       this.server.get("env") === "development" ? "dev" : "short";
     this.server.use(logger(formatsLogger));
-    this.server.use(cors({}));
+    this.server.use(cors());
     this.server.use(express.json());
+
+    this.server.use(function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      next();
+    });
+
   }
 
   initSequelizeModels() {
