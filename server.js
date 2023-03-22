a5b47846ef6fbecc1b09dc018f523e7f9eecd813
Vitya Machine <vatamanenko9@gmail.com>
2023-03-22 16:26:49 +0200

    add header handler x2

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ class Server {
     this.server.use(express.json());
 
     this.server.use(function(req, res, next) {
-      res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+      res.header("Access-Control-Allow-Origin", "https://crm-nodejs.herokuapp.com"); // update to match the domain you will make the request from
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
       next();
     });
